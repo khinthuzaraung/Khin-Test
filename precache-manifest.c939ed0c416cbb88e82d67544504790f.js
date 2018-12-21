1a06191695cacfc165ffe6ca358cdcef5b5f9aac
khinthuzaraung <khinthuzaraung03@gmail.com>
2018-12-21 15:52:25 +0630

    Updates

---
+++ precache-manifest.c939ed0c416cbb88e82d67544504790f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Khin-Test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0403de02071a7b9adfe6",
+    "url": "/Khin-Test/static/js/runtime~main.0403de02.js"
+  },
+  {
+    "revision": "5d77e443a56e6d6b5dab",
+    "url": "/Khin-Test/static/js/main.5d77e443.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Khin-Test/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "5d77e443a56e6d6b5dab",
+    "url": "/Khin-Test/static/css/main.81a57558.chunk.css"
+  },
+  {
+    "revision": "be3598a3f54507ead8582ae56e7bc1fd",
+    "url": "/Khin-Test/index.html"
+  }
+];
